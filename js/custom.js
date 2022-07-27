02da562644cec8edcb96a6b20ad247c711a861a0
kiki97-dev <dltmd@1502@naver.com>
2022-07-28 03:48:41 +0900

    보그 레이아웃 작업7월28일

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    var swiper = new Swiper(".mian_slide", {
+        direction: "vertical",
+        mousewheel : true,
+        speed : 1000,
+        slidesPerView : "1.4",
+        centeredSlides : true,
+        loop : true,
+        spaceBetween: 60,
+    });
+});
