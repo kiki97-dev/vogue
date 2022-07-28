08c398fce0dc39bb12ed82d0646fa98dc9a2176e
kiki97-dev <dltmd@1502@naver.com>
2022-07-28 17:41:13 +0900

    메인레이아웃완성

--- js/custom.js
+++ js/custom.js
@@ -2,10 +2,14 @@ $(document).ready(function () {
     var swiper = new Swiper(".mian_slide", {
         direction: "vertical",
         mousewheel : true,
-        speed : 1000,
+        speed : 800,
         slidesPerView : "1.4",
         centeredSlides : true,
-        loop : true,
+        loop : false,
         spaceBetween: 60,
+        navigation : {
+            nextEl : ".swiper-button-next",
+            prevEl : ".swiper-button-prev"
+        }
     });
 });
