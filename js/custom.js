68002b164d474d6c88df5de43349f262191a0c2a
kiki97-dev <dltmd@1502@naver.com>
2022-07-28 21:10:40 +0900

    끝

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,17 @@
 $(document).ready(function () {
     var swiper = new Swiper(".mian_slide", {
         direction: "vertical",
-        mousewheel : true,
-        speed : 800,
-        slidesPerView : "1.4",
-        centeredSlides : true,
-        loop : false,
+        mousewheel: true,
+        speed: 800,
+        slidesPerView: "1.4",
+        centeredSlides: true,
+        loop: false,
         spaceBetween: 60,
-        navigation : {
-            nextEl : ".swiper-button-next",
-            prevEl : ".swiper-button-prev"
-        }
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev"
+        },
+
+
     });
 });
